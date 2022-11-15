0cef03d3431c1af701ec64cfb55da1721d4f497c
baselreal <basriibrahim67067@gmail.com>
2022-11-15 14:34:12 +0700

    fix tailwind

--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
-    <div className="h-screen w-full bg-cover bg-no-repeat " style={{ backgroundImage: `url(${Image})` }}>
+    <div className="h-full w-full bg-cover bg-no-repeat " style={{ backgroundImage: `url(${Image})` }}>
       <div className="bg-[#2a263dd1] absolute h-full w-full"></div>
       <div className="flex flex-col justify-center items-center text-center h-screen ">
-        <div className="z-10 w-44 h-40 text-center justify-center items-center">
+        <div className="z-10 w-44 h-40 text-center justify-center items-center ">
           <img src={Bali} alt="" />
         </div>
         <div className="z-10">
-        <div className="z-10  mt-4 pt-10 text-center justify-center items-center font-pacifico ">
+        <div className="z-10  mt-4 pt-4 text-center justify-center items-center font-pacifico ">
           <div className="text-white pb-7 lg:text-5xl md:text-4.5xl text-4xl">A Better way to</div>
           <div className="ml-3 text-[#fcb371] lg:text-5xl md:text-4.5xl text-4xl">Travel to Bali</div>
         </div>
-        <div className="z-10 mb-14 mt-4 text-white ">Cheapest and Easier</div>
+        <div className="z-10 mb-10 mt-4 text-white ">Cheapest and Easier</div>
         <div className="z-10 lg:flex-row md:flex md:flex-row  flex flex-col lg:space-x-4 md:space-x-4 space-x-1 font-pacifico text-center justify-center items-center">
           <Link to="/about" className="p-1.5 text-center justify-center items-center box-border h-11 w-36 border-4 border-solid  border-[#fcb371] rounded-md text-[#fcb371]">
             About Us
--- src/screens/reservation/reservation.jsx
+++ src/screens/reservation/reservation.jsx
@@ -6,22 +6,22 @@ export const Reservation = () => {
   return (
     <div className="h-screen w-full bg-cover bg-center flex" style={{ backgroundImage: `url(${Rbg})` }}>
       <div className="bg-[#2a263dd1] absolute h-full w-full"></div>
-      <div className="z-10 ml-5 mt-5">
+      <div className="z-50 lg:ml-20 lg:mt-20 md:ml-20 md:mt-20 ml-5 mt-5 h-7 w-7">
         <Link to="/">
-          <img src={Back} alt="" />
+          <img className="lg:h-10 h-7" src={Back} alt="" />
         </Link>
       </div>
-      <div className="items-center text-center justify-center flex flex-col w-full z-10 -ml-10">
-        <div className="text-[#fcb371] font-normal text-5xl mb-6 leading-[5.25rem] font-pacifico">Reservations</div>
-        <input className="bg-white shadow-[0px 4px 4px rgba(0, 0, 0, 0.25)] text-sm mb-2 lg:w-96 md:w-96 w-60 h-11 pl-2 rounded-md" type="name" name="username" placeholder="Input Your Name" />
-        <input className="mt-2 lg:w-96 md:w-96 w-60 h-11 pl-2 rounded-md text-sm" type="email" name="mail" placeholder="Input Your Email" />
-        <div className="lg:mt-2 md:mt-4 lg:mb-2 lg:flex flex  lg:flex-row flex-col lg:w-96 md:w-96 lg:h-11  lg:space-x-5">
-          <input className="pl-2 lg:mt-2 md:mt-0 md:mb-2 mb-2 mt-4 text-sm rounded-md lg:mr-2 md:w-96 md:h-11 w-60 h-11 " type="tel" name="phone" placeholder="Input your phone" />
-          <input className="pl-2 lg:mb-2 md:mt-2 mt-2 text-sm rounded-md md:w-96 md:h-11 w-60 h-11" type="select" name="" placeholder="Select your nationality" />
+      <div className="items-center text-center justify-center flex flex-col w-full z-10 lg:-ml-20 md:-ml-20 -ml-10 mb-20 h-screen">
+        <div className="text-[#fcb371] font-normal lg:text-5xl text-2xl lg:mb-6 md:mb-6 mb-2 mt-5 leading-[5.25rem] font-pacifico">Reservations</div>
+        <input className="bg-white shadow-[0px 4px 4px rgba(0, 0, 0, 0.25)] text-sm mb-2 lg:w-96 md:w-96 w-60 lg:h-11 md:h-11 h-8 pl-2 rounded-md" type="name" name="username" placeholder="Input Your Name" />
+        <input className="mt-2 lg:w-96 md:w-96 w-60 lg:h-11 md:h-11 h-8 pl-2 rounded-md text-sm" type="email" name="mail" placeholder="Input Your Email" />
+        <div className="lg:mt-2 md:mt-4 lg:mb-2 lg:flex flex  lg:flex-row flex-col lg:w-96 md:w-96 lg:h-11 lg:space-x-5">
+          <input className="pl-2 lg:mt-2 md:mt-0 md:mb-2 mb-2 mt-4 text-sm rounded-md lg:mr-2 md:w-96 md:h-11 w-60 h-8 " type="tel" name="phone" placeholder="Input your phone" />
+          <input className="pl-2 lg:mb-2 md:mt-2 mt-2 text-sm rounded-md md:w-96 md:h-11 w-60 h-8" type="select" name="" placeholder="Select your nationality" />
         </div>
         <textarea className="pl-2 mt-4 mb-2 lg:w-96 md:w-96 w-60 h-11 rounded-md text-sm pt-2" name="trip" id="" cols="23" rows="4" placeholder="Explain about your trip"></textarea>
         <div className="bg-[#fcb371] lg:w-96 md:w-96 w-60 h-11 mt-4 rounded-md">
-          <div className="text-[white] text-sm justify-center items-center mt-2 font-pacifico" >submit</div>
+          <div className="text-[white] text-sm justify-center items-center mt-2 font-pacifico">submit</div>
         </div>
       </div>
     </div>
